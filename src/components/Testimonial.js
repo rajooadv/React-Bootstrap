fde8ff782fc386d24ae039606117947896a8e33f
rajooadv <rajooadv174@gmail.com>
2023-10-09 16:56:47 +0500

    fixing Testimonial Component

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -20,114 +20,9 @@ export default function OurTeam() {
             <Container id='testContainer'>
                 <Carousel >
                     <Carousel.Item>
-                        {/* <div className="client">
-                            <div className="testimonialHeader">
-                                    <img src="https://raw.githubusercontent.com/themewagon/HighTechIT/main/img/team-1.jpg" alt="" />
-                                <div className="profile">
-                                    <h1 className='clientName'>Client Name</h1>
-                                    <p>Profession</p>
-                                    <div class="d-flex pe-5">
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="bottomTes">
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed alias ipsam, atque quae, ipsa odio assumenda deleniti nostrum voluptate nihil eos repellat velit architecto eveniet? Sequi et odio quos eligendi at, est sit.</p>
-                            </div>
-                        </div>
-
-                        <div className="client">
-                            <div className="testimonialHeader">
-                                    <img src="https://raw.githubusercontent.com/themewagon/HighTechIT/main/img/team-2.jpg" alt="" />
-                                <div className="profile">
-                                    <h1 className='clientName'>Client Name</h1>
-                                    <p>Profession</p>
-                                    <div class="d-flex pe-5">
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="bottomTes">
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed alias ipsam, atque quae, ipsa odio assumenda deleniti nostrum voluptate nihil eos repellat velit architecto eveniet? Sequi et odio quos eligendi at, est sit.</p>
-                            </div>
-                        </div>
-
-
-                        <div className="client">
-                            <div className="testimonialHeader">
-                                    <img src="https://raw.githubusercontent.com/themewagon/HighTechIT/main/img/team-3.jpg" alt="" />
-                                <div className="profile">
-                                    <h1 className='clientName'>Client Name</h1>
-                                    <p>Profession</p>
-                                    <div class="d-flex pe-5">
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                        <i class="fas fa-star me-1 text-primary"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="bottomTes">
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed alias ipsam, atque quae, ipsa odio assumenda deleniti nostrum voluptate nihil eos repellat velit architecto eveniet? Sequi et odio quos eligendi at, est sit.</p>
-                            </div>
-                        </div> */}
-
-
 
                         <div className="parentOfTestimonial">
                             <div className="testIndiv">
-                                <div className="headerOfTestIndv">
-                                    <img src="https://raw.githubusercontent.com/themewagon/HighTechIT/main/img/testimonial-1.jpg" alt="" />
-                                    <div class="descOfTestIndv">
-                                        <h1>Client Name</h1>
-                                        <p>Profession</p>
-                                        <div className="starsOfTestIndv">
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="footerDescTestIndv">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, sapiente sunt. Eos cum magni ex
-                                        quisquam ipsam id.</p>
-                                </div>
-                            </div>
-
-                            <div className="testIndiv" id='secondTestIndv'>
-                                <div className="headerOfTestIndv">
-                                    <img src="https://raw.githubusercontent.com/themewagon/HighTechIT/main/img/testimonial-1.jpg" alt="" />
-                                    <div class="descOfTestIndv">
-                                        <h1>Client Name</h1>
-                                        <p>Profession</p>
-                                        <div className="starsOfTestIndv">
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="footerDescTestIndv">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, sapiente sunt. Eos cum magni ex
-                                        quisquam ipsam id.</p>
-                                </div>
-                            </div>
-
-
-                            <div className="testIndiv" id='thirdTestIndv'>
                                 <div className="headerOfTestIndv">
                                     <img src="https://raw.githubusercontent.com/themewagon/HighTechIT/main/img/testimonial-1.jpg" alt="" />
                                     <div class="descOfTestIndv">
@@ -151,73 +46,9 @@ export default function OurTeam() {
 
 
                     </Carousel.Item>
-                    <Carousel.Item>
-                        <div className="parentOfTestimonial">
-                            <div className="testIndiv">
-                                <div className="headerOfTestIndv">
-                                    <img src="https://raw.githubusercontent.com/themewagon/HighTechIT/main/img/testimonial-1.jpg" alt="" />
-                                    <div class="descOfTestIndv">
-                                        <h1>Client Name</h1>
-                                        <p>Profession</p>
-                                        <div className="starsOfTestIndv">
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="footerDescTestIndv">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, sapiente sunt. Eos cum magni ex
-                                        quisquam ipsam id.</p>
-                                </div>
-                            </div>
-
-                            <div className="testIndiv" id='secondTestIndv'>
-                                <div className="headerOfTestIndv">
-                                    <img src="https://raw.githubusercontent.com/themewagon/HighTechIT/main/img/testimonial-2.jpg" alt="" />
-                                    <div class="descOfTestIndv">
-                                        <h1>Client Name</h1>
-                                        <p>Profession</p>
-                                        <div className="starsOfTestIndv">
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="footerDescTestIndv">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, sapiente sunt. Eos cum magni ex
-                                        quisquam ipsam id.</p>
-                                </div>
-                            </div>
-
-
-                            <div className="testIndiv" id='thirdTestIndv' >
-                                <div className="headerOfTestIndv">
-                                    <img src="https://raw.githubusercontent.com/themewagon/HighTechIT/main/img/testimonial-3.jpg" alt="" />
-                                    <div class="descOfTestIndv">
-                                        <h1>Client Name</h1>
-                                        <p>Profession</p>
-                                        <div className="starsOfTestIndv">
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                            <i className="fa fa-star" aria-hidden="true"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="footerDescTestIndv">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, sapiente sunt. Eos cum magni ex
-                                        quisquam ipsam id.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </Carousel.Item>
+                    {/* <Carousel.Item>
+                   
+                    </Carousel.Item> */}
                 </Carousel>
             </Container>
 
